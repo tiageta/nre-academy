b5be5f915ebbb7836623a121fdf9a6df32f39e39
Tiago <lima.tiagom@gmail.com>
2022-04-22 06:40:13 -0300

    fix jquery dependency and screen overflow

--- pbl-4/scripts/main.js
+++ pbl-4/scripts/main.js
@@ -1,11 +1,10 @@
+// Collapse questions
 const questionCards = document.getElementsByClassName("question-card");
-
 function rotateCardArrow(card, deg) {
   card.getElementsByClassName(
     "arrow-icon"
   )[0].style.transform = `rotate(${deg}deg)`;
 }
-
 function isCardCollapsed(card) {
   return Boolean(
     card.getElementsByClassName("answer")[0].getAttribute("style")
@@ -17,4 +16,12 @@ Object.values(questionCards).forEach((card) => {
     const deg = isCardCollapsed(card) ? 180 : 0;
     rotateCardArrow(card, deg);
   };
+});
+
+// Modal popup
+const registerButtons = document.getElementsByClassName("button");
+const registerModal = new bootstrap.Modal("#modal");
+
+Object.values(registerButtons).forEach((button) => {
+  button.onclick = () => registerModal.toggle();
 });
